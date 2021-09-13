5dfe2339968d554691e86b9d1f11f593fab339d5
Yash Pratapwar <yash.codes22@gmail.com>
2021-09-13 11:00:20 +0530

    resolved CSS integration issue.

--- apprun.py
+++ apprun.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
