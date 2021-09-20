29ed6bc0eeb469ec7c38158b99c383fadfc429ed
Yash Pratapwar <yash.codes22@gmail.com>
2021-09-20 10:57:29 +0530

    Database added and works good.

--- apprun.py
+++ apprun.py
@@ -1,10 +1,42 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from flask_login import UserMixin
+# from flask_user import roles_required
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///adfluencer.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-
+class user_advt(db.Model, UserMixin):
+    __tablename__ = 'user_advt'
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(150), unique=False)
+    acc_handler_name = db.Column(db.String(150))
+    acc_handler_desig = db.Column(db.String(150))
+    comp_website = db.Column(db.String(150))
+    ph_no = db.Column(db.Integer)
+    comp_email = db.Column(db.String(150))
+    ah_email = db.Column(db.String(150))
+    pswd1 = db.Column(db.String(150))
+    pswd2 = db.Column(db.String(150))
+    gender = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+class user_infl(db.Model, UserMixin):
+    __tablename__ = 'user_infl'
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(150), unique=True)
+    acc_handler_name = db.Column(db.String(150))
+    acc_handler_desig = db.Column(db.String(150))
+    comp_website = db.Column(db.String(150))
+    ph_no = db.Column(db.Integer)
+    comp_email = db.Column(db.String(150))
+    ah_email = db.Column(db.String(150))
+    ah_email = db.Column(db.String(150))
+    pswd1 = db.Column(db.String(150))
+    pswd2 = db.Column(db.String(150))
+    gender = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
 @app.route('/')
 def home():
@@ -14,21 +46,46 @@ def home():
 def login():
     return render_template('login.html')
 
+
+
+# @app.route('/advt/dashboard')    # @route() must always be the outer-most decorator
+# @login_required
+# @roles_required('advt')
+# def advt_dashboard():
+#     # render the admin dashboard
+#     return render_template('advt_dashboard.html')
+
 @app.route('/register')
 def register():
     return render_template('registration.html')
 
-@app.route('/adv_regis')
+@app.route('/adv_regis', methods=['GET', 'POST'])
 def adv_regis():
-    return render_template('advertiser-registration.html')
+    if request.method == 'POST':
+        company_name = request.form.get("company_name")
+        acc_handler_name = request.form.get("acc_handler_name")
+        acc_handler_desig = request.form.get("acc_handler_desig")
+        comp_website = request.form.get("comp_website")
+        ph_no = request.form.get("ph_no")
+        comp_email = request.form.get("comp_email")
+        ah_email = request.form.get("ah_email")
+        pswd1 = request.form.get("pswd1")
+        pswd2 = request.form.get("pswd2")
+        gender = request.form["gender"]
+        adv_regis = user_advt(company_name=company_name,acc_handler_name=acc_handler_name,acc_handler_desig=acc_handler_desig,
+        comp_website=comp_website, ph_no=ph_no, comp_email=comp_email, ah_email=ah_email,pswd1=pswd1,pswd2=pswd2,gender=gender )
+        db.session.add(adv_regis)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('advertiser-registration.html')
 
-@app.route('/inf_regis')
+@app.route('/inf_regis', methods=['GET', 'POST'])
 def inf_regis():
-    return render_template('influencer-registration.html')
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    else:
+        return render_template('influencer-registration.html')
 
-
-
-
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/form_validation.js
@@ -0,0 +1,82 @@
+function validateform() {
+    var c_name = document.registration.company_name.value;
+    var ah_name = document.registration.acc_handler_name.value;
+    var ah_desig = document.registration.acc_handler_desig.value;
+    var num = document.registration.rollno.value;
+    var pw1 = document.registration.pswd1.value;
+    var pw2 = document.registration.pswd2.value;
+    var dept = document.registration.r_dept.value;
+    var age = document.registration.age.value;
+    var email = document.registration.email.value;
+    var getSelectedValue = document.querySelector('input[name="gender"]:checked');
+
+
+    if (c_name == null || c_name == "") {
+        alert("Company name can't be blank");
+        // ufname.focus();
+        return false;
+    } else if (ah_name == null || ah_name == "") {
+        alert("Account handler's name can't be blank");
+        // ah_name.focus();
+        return false;
+    } else if (ah_desig == null || acc_handler_desig == "") {
+        alert("Account handler's name can't be blank");
+        // ah_name.focus();
+        return false;
+    } else if (dept == null || dept == "") {
+        alert("Please select your Department");
+        // ah_name.focus();
+        return false;
+    } else if (num == null || num == "") {
+        alert("Roll no. can't be left blank");
+        // num.focus();
+        return false;
+    } else if (num.length < 7) {
+        alert("Roll no. can't be less than 7 digits.");
+        // num.focus();
+        return false;
+    } else if (isNaN(num)) {
+        document.getElementsByName("rollno").innerHTML = "Enter numeric value only.";
+        // num.focus();
+        return false;
+    } else if (email == "") {
+        alert("Please enter a valid e-mail address.");
+        // email.focus();
+        return false;
+    } else if (pw1 == null || pw1 == "") {
+        alert("Password can't be left blank.");
+        // password.focus();
+        return false;
+    } else if (pw1.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        // password.focus();
+        return false;
+    } else if (pw2 == null || pw2 == "") {
+        alert("Please enter your password again.");
+        // password.focus();
+        return false;
+    } else if (pw2.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        // password.focus();
+        return false;
+    } else if (pw1 != pw2) {
+        alert("Password must be same!");
+        // pw1.focus();
+        return false;
+    } else if (age == null || age == "") {
+        alert("Age can't be left blank.");
+        // age.focus();
+        return false;
+    } else if (age < 18 || age > 100) {
+        alert("Age can't be less than 18 or more than 100.");
+        // age.focus();
+        return false;
+    } else if (getSelectedValue == null) {
+        alert("Please choose your gender.");
+        return false;
+    } else {
+        alert('Form Succesfully Submitted');
+        window.location.reload()
+        return true;
+    }
+}
