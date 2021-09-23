cbbad6f0384b4eee5538c17f6a983fb0d298f27e
Yash Pratapwar <yash.codes22@gmail.com>
2021-09-23 23:50:05 +0530

    form validation sorted but still require some finishing.

--- apprun.py
+++ apprun.py
@@ -79,34 +79,34 @@ def register():
 def adv_regis():
     if request.method == 'POST':
         company_name = request.form.get("company_name")
-        if len(company_name) == "":
-            flash("Company Name cannot be empty", category= "error")
         acc_handler_name = request.form.get("acc_handler_name")
-        if len(acc_handler_name) == "":
-            flash("Account Handler Name cannot be empty", category= "error")
         acc_handler_desig = request.form.get("acc_handler_desig")
-        if len(acc_handler_desig) == "":
-            flash("Account Handler's Designation cannot be empty", category= "error")
         comp_website = request.form.get("comp_website")
-        if len(comp_website) == "":
-            flash("Company Website cannot be empty", category= "error")
         ph_no = request.form.get("ph_no")
-        if len(ph_no) == "":
-            flash("Phone Number cannot be empty", category= "error")
         comp_email = request.form.get("comp_email")
-        if len(comp_email) == "":
-            flash("Company's Email Address cannot be empty", category= "error")
         ah_email = request.form.get("ah_email")
-        if len(ah_email) == "":
-            flash("Account Handler's Email cannot be empty", category= "error")
         pswd1 = request.form.get("pswd1")
-        if len(pswd1) == "":
-            flash("Password cannot be empty", category= "error")
         pswd2 = request.form.get("pswd2")
-        if len(pswd2) == "":
-            flash("Password cannot be empty", category= "error")
         gender = request.form["gender"]
-        if len(gender) == "":
+        if (company_name == "" or company_name == None) :
+            flash("Company Name cannot be empty", category= "error")
+        elif (acc_handler_name) == "":
+            flash("Account Handler Name cannot be empty", category= "error")
+        elif (acc_handler_desig) == "":
+            flash("Account Handler's Designation cannot be empty", category= "error")
+        elif (comp_website) == "":
+            flash("Company Website cannot be empty", category= "error")
+        elif (ph_no) == "":
+            flash("Phone Number cannot be empty", category= "error")
+        elif (comp_email) == "":
+            flash("Company's Email Address cannot be empty", category= "error")
+        elif (ah_email) == "":
+            flash("Account Handler's Email cannot be empty", category= "error")
+        elif (pswd1) == "":
+            flash("Password cannot be empty", category= "error")
+        elif (pswd2) == "":
+            flash("Password cannot be empty", category= "error")
+        elif (gender) == "":
             flash("Please Select a Gender", category= "error")
         else:
             user = user_advt.query.filter_by(comp_email=comp_email).first()
@@ -119,7 +119,7 @@ def adv_regis():
                 comp_website=comp_website, ph_no=ph_no, comp_email=comp_email, ah_email=ah_email,password=hashed_password,gender=gender )
                 db.session.add(adv_regis_user)
                 db.session.commit()
-                return redirect(url_for('home', database = user_advt.query.order_by(user_advt.total_score.desc()).all()))
+                return redirect(url_for('home'))
     return render_template('advertiser-registration.html')
 
 @app.route('/inf_regis', methods=['GET', 'POST'])
