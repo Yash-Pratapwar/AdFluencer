2245a069a5ae093b6256f38cfbb9d04f2303d1c4
Yash Pratapwar <yash.codes22@gmail.com>
2021-09-28 16:01:44 +0530

    Advertiser and Influencer login working seamlessly.

--- apprun.py
+++ apprun.py
@@ -2,16 +2,21 @@ from flask import Flask, request, render_template, url_for, redirect, flash
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_login import UserMixin
+from flask_login import UserMixin, login_manager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
 # from flask_bcrypt import Bcrypt
 
 # from flask_user import roles_required
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///adfluencer.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'adfluencer@secret'
+app.secret_key = 'b8f3aabd290888870cc64c5ab34d0484'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
 # bcrypt = Bcrypt(app)
 
 class user_advt(db.Model, UserMixin):
@@ -39,8 +44,13 @@ class user_infl(db.Model, UserMixin):
     inf_email = db.Column(db.String(150))
     password = db.Column(db.String(150))
     # pswd2 = db.Column(db.String(150))
+    age = db.Column(db.Integer)
     gender = db.Column(db.String(150))
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+
+
+
 
 @app.route('/')
 def home():
@@ -51,25 +61,50 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('pswd')
-        
-        user = user_advt.query.filter_by(comp_email=email).first()
-        if user:
+        user_a = user_advt.query.filter_by(comp_email=email).first()
+        user_i = user_infl.query.filter_by(inf_email=email).first()
+        if user_a:
             
-            if check_password_hash(user.password, password):
+            if check_password_hash(user_a.password, password):
                 flash('Logged in successfully!', category='success')
-                return render_template('advt_dashboard.html')
+                login_user(user_a, remember=True)
+                return redirect('/advt/dashboard')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        
+        elif user_i:
+            if check_password_hash(user_i.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user_i, remember=True)
+                return redirect('/infl/dashboard')
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist', category='error')
     return render_template('login.html')
 
-# @app.route('/advt/dashboard')    # @route() must always be the outer-most decorator
-# @login_required
-# @roles_required('advt')
-# def advt_dashboard():
-#     # render the admin dashboard
-#     return render_template('advt_dashboard.html')
+@login_manager.user_loader
+def load_user(id):
+    return user_advt.query.get(int(id)) or user_infl.query.get(int(id))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("logged out successfully!", category='success')
+    return redirect(url_for('home'))
+
+@app.route('/advt/dashboard')    # @route() must always be the outer-most decorator
+@login_required
+def advt_dashboard():
+    # render the admin dashboard
+    return render_template('advt_dashboard.html')
+
+@app.route('/infl/dashboard')    # @route() must always be the outer-most decorator
+@login_required
+def infl_dashboard():
+    # render the admin dashboard
+    return render_template('infl_dashboard.html')
 
 @app.route('/register')
 def register():
@@ -114,11 +149,13 @@ def adv_regis():
                 flash('Email already exists.', category='error')
                 return render_template('advertiser-registration.html')
             else:
-                hashed_password = generate_password_hash(pswd1)    
-                adv_regis_user = user_advt(company_name=company_name,acc_handler_name=acc_handler_name,acc_handler_desig=acc_handler_desig,
-                comp_website=comp_website, ph_no=ph_no, comp_email=comp_email, ah_email=ah_email,password=hashed_password,gender=gender )
+                hashed_password = generate_password_hash(pswd1, method='sha256')    
+                adv_regis_user = user_advt(company_name=company_name,acc_handler_name=acc_handler_name,
+                acc_handler_desig=acc_handler_desig,comp_website=comp_website, ph_no=ph_no, comp_email=comp_email, 
+                ah_email=ah_email,password=hashed_password,gender=gender )
                 db.session.add(adv_regis_user)
                 db.session.commit()
+                flash('Account created!', category='success')
                 return redirect(url_for('home'))
     return render_template('advertiser-registration.html')
 
@@ -132,16 +169,44 @@ def inf_regis():
         ph_no = request.form.get("ph_no")
         inf_email = request.form.get("inf_email")
         # ah_email = request.form.get("ah_email")
+        age = request.form.get("age")
         pswd1 = request.form.get("pswd1")
         pswd2 = request.form.get("pswd2")
         gender = request.form["gender"]
-        inf_regis_user = user_infl(fname=fname,lname=lname,smh=smh,
-        ph_no=ph_no, inf_email=inf_email, pswd1=generate_password_hash(pswd1, method='sha256'), gender=gender )
-        db.session.add(inf_regis_user)
-        db.session.commit()
-        return redirect(url_for('home'))
-    else:
-        return render_template('influencer-registration.html')
+        if (fname == "" or fname == None) :
+            flash("First Name cannot be empty", category= "error")
+        elif (lname) == "":
+            flash("Last Name cannot be empty", category= "error")
+        elif (smh) == "":
+            flash("Please enter your social media handle URL (in format: https://www.sample.com)", category= "error")
+        elif (ph_no) == "":
+            flash("Phone Number cannot be empty", category= "error")
+        elif (inf_email) == "":
+            flash("Influencer's Email Address cannot be empty", category= "error")
+        elif (pswd1) == "":
+            flash("Password cannot be empty", category= "error")
+        elif (pswd2) == "":
+            flash("Password cannot be empty", category= "error")
+        elif (age) == "":
+            flash("Please enter your age", category= "error")
+        elif (pswd1 != pswd2):
+            flash("Passwords should match", category= "error")
+        elif (gender) == "":
+            flash("Please Select a Gender", category= "error")
+        else:
+            user = user_infl.query.filter_by(inf_email=inf_email).first()
+            if user:
+                flash('Email already exists.', category='error')
+                return render_template('influencer-registration.html')
+            else:
+                hashed_password = generate_password_hash(pswd1, method='sha256')    
+                inf_regis_user = user_infl(fname=fname,lname=lname,smh=smh, 
+                ph_no=ph_no, inf_email=inf_email, password=hashed_password,age=age, gender=gender )
+                db.session.add(inf_regis_user)
+                db.session.commit()
+                flash('Account created!', category='success')
+                return redirect(url_for('home'))
+    return render_template('influencer-registration.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
